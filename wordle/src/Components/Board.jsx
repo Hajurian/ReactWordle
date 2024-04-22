b0de5ebd9e9aa2f58d7d429cde549dab1dd4380a
Hajurian <julianmoet1@gmail.com>
2024-04-22 19:55:57 -0400

    Added word txt file and selection logic

--- wordle/src/Components/Board.jsx
+++ wordle/src/Components/Board.jsx
@@ -1,18 +1,36 @@
 import { useEffect, useState } from "react";
-import Letter from "./Letter";
+import Guess from "./Guess";
+import '../Styles/Board.css'
 
-function Board() {
+function Board(props) {
     const [guess, setGuess] = useState('');
-    const defaultBoard = ['','','','','']
+    const [current, setCurrent] = useState(0);
+    const defaultBoard = ['','','','','','']
+    const [previous, setPrevious] = useState([]);
     const handleKeyDown = (event) => {
+        if (event.key == 'Enter') {
+            if (guess.length != 5) {
+                return;
+            }
+            handleGuess(guess);
+            setCurrent(current + 1);
+        }
         if (event.key == 'Backspace') {
             setGuess(guess.substring(0, guess.length - 1))
+            return;
+        }
+        //no special characters
+        if (event.key.length > 1) {
             return;
         }
         if (guess.length >= 5){
             return;
         } 
         setGuess(guess + event.key)
+    }
+    const handleGuess = (guess) => {
+        console.log(guess);
+        setPrevious([...previous, guess]);
     }
     useEffect(() => {
         window.addEventListener('keydown', handleKeyDown);
@@ -24,11 +42,9 @@ function Board() {
     return(
         <>
             <div className="board">
-                <div className="row">
-                    {defaultBoard.map((letter, i) => {
-                        
-                    })}
-                </div>
+                {defaultBoard.map((g, i) => {
+                    return <Guess key={i} guess={guess} previous={previous} position={i} currentPos={current} />    
+                })}
             </div>
         </>
     )
